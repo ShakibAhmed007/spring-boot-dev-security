9a64ab557ae8a2fe40d2aa5fc0111535c02e93ad
unknown <shakibahmed09112017@gmail.com>
2024-08-19 11:41:23 +0600

    authentication changes

---
+++ section-12-Method level security/springsecurity/src/main/java/com/example/springsecurity/login/model/LoginRequestDTO.java
@@ -0,0 +1,17 @@
+package com.example.springsecurity.login.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+
+    private String userName;
+    private String password;
+}
---
+++ section-12-Method level security/springsecurity/src/main/java/com/example/springsecurity/login/model/LoginResponseDTO.java
@@ -0,0 +1,17 @@
+package com.example.springsecurity.login.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+
+    private String status;
+    private String jwt;
+}
